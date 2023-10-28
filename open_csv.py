f534799436d844999a0252bb768bc60282c339a8
andres2995 <loperandresfl1@gmail.com>
2023-10-28 10:58:01 -0500

    Finished the 'read' function with the creation of a list of dictionaries that contains each row of the file.

--- open_csv.py
+++ open_csv.py
@@ -3,13 +3,24 @@ import csv
 file = "global-shark-attack.csv"
 
 def read(file):
+    """
+    This function reads data from a CSV file and returns a list of dictionaries,
+    where each dictionary represents a row of data with keys from the CSV header.
+
+    :param file: The path to the CSV file to be read.
+    :type file: str
+
+    :return: A list of dictionaries containing the CSV data.
+    :rtype: list[dict]
+    """
     with open (file, 'r', encoding='utf-8') as csv_read:
         reader = csv.reader(csv_read, delimiter=';')
         header = next(reader)
-        print(list(reader))
-      
+        data = [dict (zip(header, row)) for row in reader]
+       
 
-read(file)
+    return data
+
 
 if __name__ == '__main__':
-    read(file)
+    print(read(file))
